a163cf2cedc9c455ac6f1228f93e4599ccbf7069
gabrielreiss <gabrielreissdecastro@gmail.com>
2021-10-22 00:41:37 -0300

    Acrescentando análise Markowitz

--- app.py
+++ app.py
@@ -4,8 +4,10 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-import pyfolio as pf
+#import pyfolio as pf
 import datetime as dt
+import numpy as np
+from src.python.markowitz import fronteira_eficiente
 
 warnings.filterwarnings('ignore')
 
@@ -15,11 +17,8 @@ DATA_DIR = os.path.join(BASE_DIR,'data')
 SQL_DIR = os.path.join(BASE_DIR, 'src', 'sql')
 PYTHON_DIR = os.path.join(BASE_DIR, 'src', 'python')
 
-os.environ['TZ'] = 'UTC'
-
 #carrega os dados
 data = pd.read_csv(os.path.join(DATA_DIR, 'data.csv'),index_col="Date", infer_datetime_format= True, parse_dates=['Date'])
-print(data)
 
 #main function
 if __name__ == "__main__":
@@ -31,12 +30,10 @@ if __name__ == "__main__":
     options_tickers = st.sidebar.multiselect("Lista de Criptos", list_of_tickers, default=['BTC', 'ETH', 'LTC', 'XRP', 'DASH', 'SC'])
 
     #filtra os dados pelo período
-    periodo = st.sidebar.slider('Período analisado', 1, 5, value = 5)
+    periodo = st.sidebar.slider('Período analisado', 1, 5, value = 1)
     start = dt.datetime.now() - dt.timedelta(days=(365 * periodo))
     end = dt.datetime.now()
     data_filtrada = data[options_tickers].loc[start:end]
-
-    os.environ['TZ'] = 'UTC'
 
     #Apresenta um resumo em tabelas
     st.header('Histórico de preços em Dólar')
@@ -44,19 +41,44 @@ if __name__ == "__main__":
 
     #Imprime gráfico dos valores
     for ticket in options_tickers:
-        plt.figure(figsize=(12,5))
+        fig = plt.figure(figsize=(12,5))
         ax = sns.lineplot(data=data_filtrada, x=data_filtrada.index, y= ticket,palette="tab10", linewidth=2.5)
         ax.set(xlabel='Ano', ylabel='USD')
         plt.title(f'Evolução do {ticket}')
-        st.pyplot()
+        st.pyplot(fig)
 
     st.header('Variação Diária')
     data_rend_diarios = data_filtrada[options_tickers].pct_change()
     data_rend_diarios = data_rend_diarios[1:]
     st.dataframe(data_rend_diarios)
 
+    #matriz de correlação
+    st.subheader('Matriz de Correlação')
+    correlação = data_rend_diarios.corr(method = "pearson")
+    fig = plt.figure(figsize=(12,5))
+    sns.heatmap(correlação, annot=True, cmap="coolwarm")
+    st.pyplot(fig)
+
+
     st.header('Retorno Acumulado')
     retorno_acumulado = (1 + data_rend_diarios).cumprod()
     retorno_acumulado = retorno_acumulado[1:]
     st.dataframe(retorno_acumulado)
+
+
+    st.title("Fronteira Eficiente de Markowitz")
+    sharpe_maximo, pesos, ret_arr, vol_arr, sharpe_arr, max_sr_ret, max_sr_vol = fronteira_eficiente(data_rend_diarios)
+
+    fig = plt.figure(figsize=(12,8))
+    plt.scatter(vol_arr, ret_arr, c=sharpe_arr, cmap='viridis')
+    plt.colorbar(label='Sharpe Ratio')
+    plt.xlabel('Volatility')
+    plt.ylabel('Return')
+    plt.scatter(max_sr_vol, max_sr_ret,c='red', s=50) # red dot
+    st.pyplot(fig)
+
+    st.table(pesos)
+    st.write(f'Retorno da carteira: {max_sr_ret:.2f}')
+    st.write(f'Volatilidade da carteira: {max_sr_vol:.2f}')
+    st.write(f'O sharp da carteira: {sharpe_maximo:.2f}')
 
---
+++ src/python/markowitz.py
@@ -0,0 +1,35 @@
+import numpy as np
+import pandas as pd
+
+def fronteira_eficiente(data_rend_diarios):
+    np.random.seed(42)
+    num_ports = 6000
+    all_weights = np.zeros((num_ports, len(data_rend_diarios.columns)))
+    ret_arr = np.zeros(num_ports)
+    vol_arr = np.zeros(num_ports)
+    sharpe_arr = np.zeros(num_ports)
+
+    for x in range(num_ports):
+        # Weights
+        weights = np.array(np.random.random(len(data_rend_diarios.columns)))
+        weights = weights/np.sum(weights)
+        
+        # Save weights
+        all_weights[x,:] = weights
+        
+        # Expected return
+        ret_arr[x] = np.sum( (data_rend_diarios.mean() * weights * 252))
+        
+        # Expected volatility
+        vol_arr[x] = np.sqrt(np.dot(weights.T, np.dot(data_rend_diarios.cov()*252, weights)))
+        
+        # Sharpe Ratio
+        sharpe_arr[x] = ret_arr[x]/vol_arr[x]
+
+    max_sr_ret = ret_arr[sharpe_arr.argmax()]
+    max_sr_vol = vol_arr[sharpe_arr.argmax()]
+
+    pesos = pd.DataFrame(all_weights[sharpe_arr.argmax(),:], index =  list(data_rend_diarios.columns), columns = ['Pesos'])
+    
+    return sharpe_arr.max(), pesos, ret_arr, vol_arr, sharpe_arr, max_sr_ret, max_sr_vol
+
